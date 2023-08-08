fa291465cab6a37c79bf34d57217d03d778383ad
SergioRon2 <rondonjr2005@gmail.com>
2023-08-07 23:54:24 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const algoritmo = () =>{
+    let numero = Math.floor(Math.random() * 1000);
+    random = document.getElementById('numero-random')
+
+    random.innerHTML = numero;
+}
+
+let numeroEntrada = getElementById('entrada')
+
+
+
